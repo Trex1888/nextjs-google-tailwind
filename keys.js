94b2d04dc4955a9d04dad5bd19eb9a64c7c27b8c
Trex1888 <harpua123@gmail.com>
2021-04-13 00:54:09 -0500

    2nd commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+const API_KEY = process.env.NEXT_APP_API_KEY;
+const CONTEXT_KEY = process.env.NEXT_APP_CONTEXT_KEY;
+
+export { API_KEY, CONTEXT_KEY };
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,25 @@ import Image from "next/image";
 import Avatar from "../components/Avatar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { SearchIcon } from "@heroicons/react/outline";
+import Footer from "../components/Footer";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>Google Tailwind CSS</title>
         <link rel="icon" href="/favicon.ico" />
@@ -28,7 +43,7 @@ export default function Home() {
         </div>
       </header>
 
-      <form className="flex flex-col items-center mt-44 flex-grow">
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt="pic"
@@ -40,18 +55,29 @@ export default function Home() {
           rounded-full border border-gray-200 px-5 py-5 items-center sm:max-w-xl lg:max-w-2xl"
         >
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
+          />
           <MicrophoneIcon className="h-5" />
         </div>
 
-        <div>
-          <button className="btn">Google Search</button>
+        <div
+          className="flex flex-col w-1/2 space-y-2 justify-center mt-8 
+          sm:space-y-0 sm:flex-row sm:space-x-4"
+        >
+          <button onClick={search} className="btn">
+            Google Search
+          </button>
 
-          <button className="btn">Coding Nextjs</button>
+          <button onClick={search} className="btn">
+            Coding Nextjs
+          </button>
         </div>
       </form>
 
-      <footer>{/* shit */}</footer>
+      <Footer />
     </div>
   );
 }
