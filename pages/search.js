e14cfe7f7339d612df8b3fa1a70067a59d2f2cef
Trex1888 <harpua123@gmail.com>
2021-04-14 00:30:01 -0500

    3rd commit

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,22 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
-function Search() {
+function Search({ results }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <SearchResults results={results} />
     </div>
   );
 }
@@ -18,4 +25,17 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useData = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
